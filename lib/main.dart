58105a396d78a33f3b95819b2acd3e2fad6e2287
Takashi Aihara <runtakashi@hotmail.com>
2021-07-25 22:20:44 +0900

    add navigation 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'utilities/selectuiplatform.dart';
+import 'platformsetting.dart';
+import 'screen/somescreen.dart';
+import 'screen/secondscreen.dart';
 
 void main() {
   // debugPrintBeginFrameBanner = true;
@@ -29,76 +31,16 @@ class AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return SomeScreen(title: 'sample');
+    return PlatformSetting(
+      routes: {
+        SomeScreen.name: (context) => SomeScreen(title: 'some screen'),
+        SecondScreen.name: (context) => SecondScreen(),
+      },
+    );
   }
 
   static const _store = GlobalObjectKey<AppState>('state');
   static AppState get draw {
     return _store.currentState!;
-  }
-}
-
-class SomeScreen extends StatelessWidget {
-  SomeScreen({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              SomeWidget(),
-              ValueListenableBuilder<int>(
-                // valueListenable:
-                //     GlobalObjectKey<AppState>('state').currentState!.counter,
-                valueListenable: AppState.draw.counter,
-                builder: (context, int counter, child) {
-                  return Text(
-                    '$counter',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: GestureDetector(
-          onTap: () {
-            //You can call _store, but I don't want you to use it.
-            // AppState._store!.currentState!.counter.value++;
-            AppState.draw.counter.value++;
-          },
-          child: const Icon(Icons.add),
-        ),
-      ),
-    );
-  }
-}
-
-class SomeWidget extends StatelessWidget {
-  const SomeWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SelectUIPlatform(
-      platformIOS:
-          Container(width: 100, height: 100, color: Colors.lightBlueAccent),
-      platformWeb: Container(width: 20, height: 20, color: Colors.red),
-      defaultUI: Container(width: 20, height: 20, color: Colors.black),
-    );
   }
 }
